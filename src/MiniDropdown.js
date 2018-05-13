7565f7101c5089fdba1698b3753ccc64a14ba090
Oleksandr Pastukhov <flashka.ua@gmail.com>
2018-05-13 18:28:30 +0200

    added prop types

--- src/MiniDropdown.js
+++ src/MiniDropdown.js
@@ -1,25 +1,35 @@
 import React, { cloneElement } from 'react'
 import { Overlay } from 'react-overlays'
-import { compose, withState, withHandlers } from 'recompose'
+import { element, bool, any } from 'prop-types'
+import { compose, withState, withHandlers, defaultProps, setPropTypes } from 'recompose'
 
 const Content = ({ children }) => <div style={{ position: 'absolute' }} children={children} />
 
 const MiniDropdown = ({ component, disabled, onClick, ref, target, show, onHide, children }) => (
   <div>
     {cloneElement(component, { disabled, onClick, ref })}
-    <Overlay rootClose placement="bottom" {...{ target , show, onHide }}>
+    <Overlay rootClose placement="bottom" {...{ target, show, onHide }}>
       <Content children={children} />
     </Overlay>
   </div>
 )
 
 const enhanceDropdown = compose(
+  setPropTypes({
+    component: element.isRequired,
+    disabled: bool,
+    children: any,
+  }),
+  defaultProps({
+    disabled: false,
+    children: null,
+  }),
   withState('show', 'setShow', false),
   withState('target', 'ref', null),
   withHandlers({
     onClick: ({ disabled, setShow, show }) => () => !disabled && setShow(!show),
-    onHide: ({ show, setShow }) => () => setShow(!show),
-  }),
+    onHide: ({ show, setShow }) => () => setShow(!show)
+  })
 )
 
 export default enhanceDropdown(MiniDropdown)
